c0eb5e47b174fdfa0ae620d5baab7c86e0dc51be
KhaledShehataA <khaled.ah.shehata@gmail.com>
2021-08-07 03:50:48 +0200

    materialize setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import React, { useEffect } from 'react';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
 import './App.css';
 
 const App = () => {
+	useEffect(() => {
+		// Init materialize JS
+		M.AutoInit();
+	});
 	return <div className='App'></div>;
 };
 
